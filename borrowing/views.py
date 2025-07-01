0e1616f9ec152ecd96c7c2710dab13ff097ba479
Timur Khvat <timurkhvat@gmail.com>
2025-07-01 18:28:20 +0200

    Implement read-only BorrowingViewSet with dynamic serializer for retrieve

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingSerializer, BorrowingDetailSerializer
+
+
+class BorrowingViewSet(mixins.RetrieveModelMixin,
+                       mixins.ListModelMixin,
+                       GenericViewSet):
+    queryset = Borrowing.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return BorrowingSerializer
