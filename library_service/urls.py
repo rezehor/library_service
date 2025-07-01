910a1dc1e0011b9b4eb933f92ed87c7d438bfc95
Timur Khvat <timurkhvat@gmail.com>
2025-07-01 16:39:32 +0200

    Add JWT token authentication to settings and urls

--- library_service/urls.py
+++ library_service/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/book/", include("book.urls", namespace="books")),
+    path("api/user/", include("user.urls", namespace="users")),
 ]
