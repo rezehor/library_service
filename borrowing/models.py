3913bb6ecdebed3cac1c5081ed539fe7ab897261
Timur Khvat <timurkhvat@gmail.com>
2025-07-01 17:57:11 +0200

    Add the borrowing model with constraints for borrow_date, expected_return_date, and actual_return_date.

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from book.models import Book
 from library_service.settings import AUTH_USER_MODEL
@@ -7,7 +8,7 @@ from library_service.settings import AUTH_USER_MODEL
 class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
 
@@ -16,3 +17,37 @@ class Borrowing(models.Model):
 
     def __str__(self):
         return f"User({self.user.id}), Book({self.book.id}), Date({self.borrow_date}-{self.expected_return_date})"
+
+
+    @staticmethod
+    def validate_borrowing(borrow_date, expected_return_date, actual_return_date, error_to_raise):
+        if expected_return_date < borrow_date:
+            raise error_to_raise({
+                "expected_return_date": "Expected return date cannot be before borrow date."
+            })
+
+        if actual_return_date and actual_return_date < borrow_date:
+            raise error_to_raise({
+                "actual_return_date": "Actual return date cannot be before borrow date."
+            })
+
+    def clean(self):
+        self.validate_borrowing(
+            self.borrow_date,
+            self.expected_return_date,
+            self.actual_return_date,
+            ValidationError
+        )
+
+    def save(
+            self,
+            *args,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Borrowing, self).save(
+            force_insert, force_update, using, update_fields
+        )
--- library_service/settings.py
+++ library_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "book",
+    "borrowing",
     "user"
 ]
 
@@ -131,4 +132,10 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=10),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
+    "ROTATE_REFRESH_TOKENS": False,
 }
