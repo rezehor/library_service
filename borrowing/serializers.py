6cb634494b7329e70fb28070be8c548296ae16e0
Timur Khvat <timurkhvat@gmail.com>
2025-07-02 09:25:24 +0200

    Add BorrowingReturnSerializer to handle book returns

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,6 +1,7 @@
+from datetime import datetime
+
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-
 from book.serializers import BookSerializer
 from borrowing.models import Borrowing
 
@@ -24,3 +25,21 @@ class BorrowingSerializer(serializers.ModelSerializer):
 
 class BorrowingDetailSerializer(BorrowingSerializer):
     book = BookSerializer(read_only=True)
+
+
+class BorrowingReturnSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("id", "actual_return_date")
+
+    def validate(self, data):
+        if self.instance.actual_return_date:
+            raise serializers.ValidationError("Borrowing has already been returned.")
+        return data
+
+    def update(self, instance, validated_data):
+        instance.actual_return_date = datetime.now().date()
+        instance.save()
+        instance.book.inventory += 1
+        instance.book.save(update_fields=["inventory"])
+        return instance
