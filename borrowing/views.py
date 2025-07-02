89d1cb67f461fd1040837a2bee3eba734ea86593
Timur Khvat <timurkhvat@gmail.com>
2025-07-02 09:25:45 +0200

    Add return_borrowing action to BorrowingViewSet

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,8 +1,10 @@
-from rest_framework import mixins
+from rest_framework import mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer, BorrowingDetailSerializer
+from borrowing.serializers import BorrowingSerializer, BorrowingDetailSerializer, BorrowingReturnSerializer
 
 
 class BorrowingViewSet(mixins.RetrieveModelMixin,
@@ -15,7 +17,21 @@ class BorrowingViewSet(mixins.RetrieveModelMixin,
     def get_serializer_class(self):
         if self.action == "retrieve":
             return BorrowingDetailSerializer
+        elif self.action == "return_borrowing":
+            return BorrowingReturnSerializer
         return BorrowingSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="return",
+    )
+    def return_borrowing(self, request, pk=None):
+        borrowing = self.get_object()
+        serializer = BorrowingReturnSerializer(borrowing, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"detail": "Book returned successfully."}, status=status.HTTP_200_OK)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
