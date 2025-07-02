56ed91659fa2f42c2a5aed09bae7eb7265b03035
Timur Khvat <timurkhvat@gmail.com>
2025-07-02 11:44:32 +0200

    Add PaymentViewSet to payment app and allow admins to see all payments, users only their own

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payment.models import Payment
+from payment.serializers import PaymentSerializer, PaymentDetailSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser or user.is_staff:
+            return self.queryset.all()
+        return self.queryset.filter(user=user)
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+        return PaymentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
