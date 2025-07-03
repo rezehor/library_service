b3cd361e495ba425a214e545864e5e0631a65d1d
Timur Khvat <timurkhvat@gmail.com>
2025-07-03 13:29:52 +0200

    Add secret key to .env

--- library_service/settings.py
+++ library_service/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-461yokza&+c*002-7a*lkb8--gshnb-74v@zb&=b$5=1@0j(ll"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
